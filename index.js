d4cb0ec1265a17daf9fc2fad8fa0781c28ae5143
Aldair <alarriolasa@ittepic.edu.mx>
2019-12-16 14:22:24 -0700

    primera subida

---
+++ index.js
@@ -0,0 +1,12 @@
+const {Builder, By, Key, util} = require("selenium-webdriver");
+async function prueba(){
+    let driver = await new Builder().forBrowser("firefox").build(); //navegador web a utilizar
+    await driver.get("https://www.newocr.com/"); // página web a la que se desea accesar
+    await driver.findElement(By.id("userfile")).sendKeys('C:\\Selenium\\imagenes\\prueba.jpg'); // documento a subir
+    await driver.findElement(By.name("preview")).click();
+    await driver.findElement(By.name("ocr")).click();
+    await driver.findElement(By.className('dropdown-toggle')).click();
+    await driver.findElement(By.linkText('Plain text (TXT)')).click();
+}
+
+prueba();
