37d5e53934d537899b3603a90d56c6c31585aa9f
AldairAS <aldair1327@gmail.com>
2020-01-21 14:17:30 -0700

    Subida 2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
 async function prueba(){
-    let driver = await new Builder().forBrowser("firefox").build(); //navegador web a utilizar
+    let driver = await new Builder().forBrowser("chrome").build(); //navegador web a utilizar
     await driver.get("https://www.newocr.com/"); // página web a la que se desea accesar
-    await driver.findElement(By.id("userfile")).sendKeys('C:\\Selenium\\imagenes\\prueba.jpg'); // documento a subir
+    await driver.findElement(By.id("userfile")).sendKeys('C:/Selenium/prueba.jpg'); // documento a subir
     await driver.findElement(By.name("preview")).click();
     await driver.findElement(By.name("ocr")).click();
     await driver.findElement(By.className('dropdown-toggle')).click();
